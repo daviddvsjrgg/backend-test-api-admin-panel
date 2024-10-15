a44c02abe1b0ad53b1814470c262e7bdf93f5c0a
David <daviddwiyanto79@gmail.com>
2024-10-15 20:27:50 +0700

    asd

--- src/api/index.js
+++ src/api/index.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({
+      message: 'API - 👋🌎🌍🌏',
+    });
+  });
+
 module.exports = router;
